8af14d7584e13c1a3656328e16bd06af4b552bf3
Your Name <you@example.com>
2023-03-29 14:39:14 +0700

    Config for blocked hosting

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module PhamThiMyDung21070509SupermarketSystemFinalSubmission
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    
+    config.hosts <<"supermarket-app-production.up.railway.app"
   end
 end
