1d0f6f4ce5d8f292f51ada23e59464071264bde7
Your Name <you@example.com>
2023-04-13 00:31:31 +0700

    Add model Validation and Association

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,10 +1,15 @@
 class Staff < ApplicationRecord
-    has_many :sale
+    has_many :sales
 
     validates_presence_of :staff_name, :gender, :age, :address, :date_of_birth
     
-    validate :year_up_to_present
-    def year_up_to_present
-        errors.add(:date_of_birth, 'must not be in the future') if date_of_birth > Time.now.year
+    validates :date_of_birth, presence: true
+    validate :date_of_birth_cannot_be_in_the_future
+
+    def date_of_birth_cannot_be_in_the_future
+        if date_of_birth.present? && date_of_birth > Date.today
+        errors.add(:date_of_birth, "can't be in the future")
+        end
     end
+
 end
--- db/migrate/20230412172736_create_staffs.rb
+++ db/migrate/20230412172736_create_staffs.rb
@@ -2,7 +2,7 @@ class CreateStaffs < ActiveRecord::Migration[7.0]
   def change
     create_table :staffs do |t|
       t.string :staff_name
-      t.integer :gender
+      t.string :gender
       t.integer :age
       t.string :address
       t.date :date_of_birth
