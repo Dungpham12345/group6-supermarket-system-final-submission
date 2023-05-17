467d059ba62485c0ffdc4e984661013b061a3a88
Your Name <you@example.com>
2023-05-18 00:13:25 +0700

    Update

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,7 +2,7 @@ class Sale < ApplicationRecord
   belongs_to  :product
   belongs_to :staff
 
-  validates_presence_of :date, :quantity
+  validates_presence_of :product, :staff, :date, :quantity
   validates_numericality_of :quantity
 
   validate :date_cannot_be_in_the_future
@@ -12,12 +12,12 @@ class Sale < ApplicationRecord
   
   validate :quantity_must_be_positive
   def quantity_must_be_positive
-    errors.add(:quantity, 'must be a positive integer') if quantity.to_i <= 0
+    errors.add(:quantity, 'must be greater than 0') if quantity.to_i <= 0
   end
   
   before_save :calculate_grand_total
   # Calculate the grand total based on price and quantity
    def calculate_grand_total
-    grand_total = price.to_f * quantity.to_i
+    self.grand_total = price.to_f * quantity.to_i
   end
 end
