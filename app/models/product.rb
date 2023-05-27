9709763eb8c4e4604ce82ebf14b9892a7fbda85f
Your Name <you@example.com>
2023-05-27 13:34:22 +0700

    Final update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :supplier
   validates_presence_of :product_name, :category, :price, :supplier, :subcategory, :origin, :size
+  validates_numericality_of :price
 end
